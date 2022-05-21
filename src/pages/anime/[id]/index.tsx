0549392bb46a5c21d6d1060a667fbc25240648ba
jukibenghantachi <jukibenghantachi@gmail.com>
2022-05-21 21:02:19 +0700

    feat(detail): get query detail

--- src/pages/anime/[id]/index.tsx
+++ src/pages/anime/[id]/index.tsx
@@ -1,6 +1,9 @@
 import { NextPage } from 'next/types';
+import { useQueryPage } from '../../../hooks/useQueryPage';
 
 const AnimeDetail: NextPage = () => {
+  const { id } = useQueryPage();
+
   return <p>Detail</p>;
 };
 
